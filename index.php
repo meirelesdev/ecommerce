7c626bbf3a3b98547b0e9107a89c5357f349c9f4
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-04-24 22:04:28 -0300

    Configurando painel admin laout

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -14,6 +15,14 @@ $app->get('/', function() {
 	$page = new Page();
 	
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+	echo "Ok";
+	// $pageAdmin = new PageAdmin();
+	
+	// $pageAdmin->setTpl("index");
 
 });
 
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page{
+
+    public function __construct($opts = array(), $tpl_dir = "views/adm"){
+        
+        parent::__construct($opts, $tpl_dir); 
+    
+    }
+
+}
+
+?>
