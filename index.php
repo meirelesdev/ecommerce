ef75c85ae10aede9bd533031a2d944ab133a9d1c
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-04-29 10:23:24 -0300

    Criação CRUD categorias

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ session_start();
 
 require_once("vendor/autoload.php");
 
-
+use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
-$app = new \Slim\Slim();
+
+$app = new Slim();
 
 $app->config('debug', true);
 
@@ -16,21 +18,7 @@ $app->get('/', function() {
 	
 	$page = new Page();
 
-	$page->setTpl("index");
-
-});
-
-// Rota para listar todos os usuarios
-$app->get("/admin/users", function() {
-	// Verifica se o usuario que chamou a rota esta logado
-	User::verifyLogin();
-	// faz um select nos usuarios e retorna
-	$users = User::listAll();	
-	$page = new PageAdmin();
-	// Carrega o template users enviando a lista com todos os usuarios
-	$page->setTpl("users", array(
-		"users"=>$users
-	));
+	$page-> setTpl("index");
 
 });
 
@@ -55,6 +43,22 @@ $app->get('/admin/login', function() {
 
 	$page->setTpl('login');
 });
+
+// Rota para listar todos os usuarios
+$app->get("/admin/users", function() {
+	// Verifica se o usuario que chamou a rota esta logado
+	User::verifyLogin();
+	// faz um select nos usuarios e retorna
+	$users = User::listAll();	
+	$page = new PageAdmin();
+	// Carrega o template users enviando a lista com todos os usuarios
+	$page->setTpl("users", [
+		"users"=>$users
+	]);
+
+});
+
+
 $app->post('/admin/login', function() {
 	
 	User::login($_POST["login"], $_POST["password"]);
@@ -123,9 +127,9 @@ $app->get("/admin/users/:iduser", function($iduser) {
 
 	$page = new PageAdmin();
 
-	$page->setTpl("users-update", array(
+	$page->setTpl("users-update", [
 		"user"=>$user->getValues()
-	));
+	]);
 
 });
 
@@ -190,10 +194,10 @@ $app->get('/admin/forgot/reset', function(){
 		"footer"=>false
 	]);
 
-	$page->setTpl("forgot-reset", array(
+	$page->setTpl("forgot-reset", [
 		"name"=>$user["desperson"],
 		"code"=>$_GET["code"]
-	));
+	]);
 
 });
 
@@ -217,7 +221,85 @@ $app->post("/admin/forgot/reset", function() {
 	$page->setTpl("forgot-reset-success");
 });
 
+$app->get("/admin/categories", function(){
+	
+	User::verifyLogin();
 
+	$page = new PageAdmin();
+
+	$categories = Category::listAll();
+ 
+	$page->setTpl("categories",[
+		"categories"=>$categories
+	]);
+});
+
+$app->get("/admin/categories/create", function() {
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function() {
+	
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory) {
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+ 
+	header  ("Location: /admin/categories");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory) {
+		User::verifyLogin();
+
+	$category = new Category();
+	
+	$category->get((int)$idcategory);
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		"category"=>$category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory) {
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
 $app->run();
 
 ?>
+  
